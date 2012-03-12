402a0b5ba82bc20b8af2a53d61ea60aae4c287ad
root <root@saasbook.(none)>
2012-03-12 12:46:55 -0700

    hw2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
-
+  helper_method :sort_column, :sort_direction, :array_to_hash
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +8,19 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    column = sort_column
+    direction = sort_direction
+    @all_ratings = Movie.valid_ratings 
+    @ratings = filter_ratings
+    if (params_empty? && !session_empty? )
+      rp = redirect_params
+      puts "R PARAMS #{rp.to_s}"
+      redirect_to movies_path (rp)
+    else
+      session[:ratings] = @ratings
+      session[:sort] = column
+      @movies = @ratings.empty? ? Movie.order(column + " " + direction) : Movie.order(column + " " + direction).where(:rating => @ratings)      
+    end
   end
 
   def new
@@ -37,5 +50,48 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  private
+  
+  
+  def params_empty? 
+    !params.include?(:ratings) && !params.include?(:sort)
+  end
+  
+  def sort_column(ar=nil)
+    ar = params if ar == nil
+    Movie.column_names.include?(ar[:sort]) ? ar[:sort] : "title"
+  end
+    
+  def filter_ratings(ar=nil)
+    ar = params if ar == nil
+    r = ar[:ratings]
+    r == nil ? [] : r.keys & Movie.valid_ratings
+  end
+  
+  def session_ratings()
+    r = session[:ratings]
+    r == nil ? [] : r & Movie.valid_ratings
+    array_to_hash(r)
+  end
+  
+  def redirect_params
+   {:sort => sort_column(session), :ratings => session_ratings}  
+  end
+  
+  def session_empty?
+    !session.include?(:ratings) && !session.include?(:sort)
+  end
+  
+  def sort_direction
+    #%w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    "asc"
+  end
+  
+    
+  def array_to_hash(a)
+    Hash[*a.collect { |v| [v, v*2] }.flatten]
+  end
+
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  def th_sortable(column, title = nil, ratings=[])
+    title ||= column.titleize
+    current = column == sort_column
+    css_class = current ? "hilite" : nil
+    id_name = "#{column}_header"
+    direction = current && sort_direction == "asc" ? "desc" : "asc"
+    capture_haml do
+      haml_tag :th, :class => css_class do
+        haml_concat link_to title, {:sort => column, :direction => direction, :ratings => array_to_hash(ratings)}, {:id => id_name}
+      end
+    end
+  end
+
+
 end
